166e1d0b5a98b879190da982d18d8a57e4dea663
amgdwafik-amw <amgdwafik.amw@gmail.com>
2025-12-16 05:01:31 +0200

    Add files via upload

--- build.sh
+++ build.sh
@@ -2,6 +2,20 @@
 # exit on error
 set -o errexit
 
+echo "--- Debugging File Structure ---"
+ls -la
+if [ -d "oms_backend" ]; then
+    echo "oms_backend exists"
+    ls -la oms_backend
+else
+    echo "ERROR: oms_backend directory MISSING!"
+fi
+echo "--------------------------------"
+
 pip install -r requirements.txt
+
+# Fix for module not found
+export PYTHONPATH=$PYTHONPATH:.
+
 python manage.py collectstatic --no-input
 python manage.py migrate
